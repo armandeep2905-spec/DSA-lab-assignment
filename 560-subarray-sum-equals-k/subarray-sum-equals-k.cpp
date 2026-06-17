0c568d2a8545878a6d277d7783bcd32eb8fa7a05
armandeep2905-spec <armandeep2905@gmail.com>
2026-06-17 15:15:12 +0530

    Time: 91 ms (21.75%) | Memory: 54 MB (5.44%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-//github push check
+ // committing on leetcode
     int subarraySum(vector<int>& nums, int k) {
-      map<int , int > mp ;
+      map<int ,int > mp ;
       mp[0]= 1 ;
       int prefixSum = 0 ;
       int count = 0 ;
